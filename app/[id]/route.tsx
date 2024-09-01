9651ce484ec8e25f12317f3a6e9b14623dc77295
pliavi <pliavi.rpgm@gmail.com>
2024-09-01 12:54:52 -0300

    feat: add 404 page

--- app/[id]/route.tsx
+++ app/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { ShortcutRepository } from "@/lib/repositories/ShortcutRepository";
+import { notFound } from "next/navigation";
 import { NextResponse } from "next/server";
 
 type Context = {
@@ -9,9 +10,12 @@ type Context = {
 
 export async function GET(_request: Request, { params }: Context) {
   const { id } = params;
+  try {
+    const shortcut = await ShortcutRepository.get(id);
+    const urlRow = await ShortcutRepository.getNextUrl(shortcut);
 
-  const shortcut = await ShortcutRepository.get(id);
-  const urlRow = await ShortcutRepository.getNextUrl(shortcut);
-
-  return NextResponse.redirect(urlRow.url);
+    return NextResponse.redirect(urlRow.url);
+  } catch (error) {
+    return NextResponse.redirect(new URL("/404", _request.url));
+  }
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,13 @@ export default function RootLayout({
         <div className="flex flex-col w-dvw h-dvh relative bg-gray-900 items-center justify-center overflow-hidden text-white">
           <RSBackgroundBubbles />
           <div className="fixed inset-0 border-4 border-l-pink-500 border-r-orange-500 border-b-cyan-500 border-t-purple-500 border-opacity-25"></div>
-          <div className="fixed inset-0 backdrop-blur-3xl z-[2]"></div>
-          <div className="fixed inset-0 z-[4] bg-[url('/background.jpg')] bg-cover bg-center mix-blend-color-dodge opacity-25"></div>
-          <div className="fixed inset-0 h-dvh sm:inset-auto sm:relative z-10 container md:max-w-[1024px] mx-auto sm:h-4/5 flex flex-col items-center justify-stretch sm:justify-center sm:gap-6">
+          <div className="fixed inset-0 backdrop-blur-3xl z-10"></div>
+          <div className="fixed inset-0 z-20 bg-[url('/background.jpg')] bg-cover bg-center mix-blend-color-dodge opacity-25"></div>
+          <div className="fixed inset-0 h-dvh sm:inset-auto sm:relative z-30 container md:max-w-[1024px] mx-auto sm:max-h-[75%] sm:h-fit flex flex-col items-center justify-stretch sm:justify-center sm:gap-6">
             <h1 className="flex-shrink-0 basis-20 flex items-center justify-center relative text-4xl sm:text-6xl text-center text-white font-black drop-shadow-xl">
               Shortprise
             </h1>
-            <div className="w-full flex-1 overflow-y-scroll sm:max-h-[90%] bg-gray-950 bg-opacity-75 sm:rounded-2xl shadow-2xl">
+            <div className="w-full flex-1 overflow-y-scroll sm:h-fit bg-gray-950 bg-opacity-75 sm:rounded-2xl shadow-2xl">
               {children}
             </div>
           </div>
